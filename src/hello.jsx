7407f10544a656b194cbd9daf1477da8698255d6
lianwei <lianwei1994@163.com>
2019-02-23 22:03:11 +0800

    增加项目其他代码

--- src/hello.jsx
+++ src/hello.jsx
@@ -10,14 +10,14 @@ import React from "react";
 import Data from "./data";
 import "./css/style.less";
 
-export default class Hello extends React.Component{
-	render(){
-		return(
+export default class Hello extends React.Component {
+	render() {
+		return (
 			<div>
-				<p className="ptext">{ Data.name }</p>
+				<p className="ptext">{Data.name}</p>
 				<div className="nav">
 					<ul>
-            <li>哈哈1</li>
+						<li>哈哈1</li>
 						<li>哈哈2</li>
 						<li>哈哈3</li>
 						<li>哈哈4</li>
